7cc891d3c3fe1c95ac15be3bb43aef73cc303ca4
Badri Vishal <79777609+MrBlueBird2@users.noreply.github.com>
2021-10-18 08:07:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  function portfolio_flask() {
    description = "Badri Vishal's Personal Portfolio Website Built with Python Framework Flask Bootstrap and ❤️";
-   expected_date = "23/10/2021';
+   expected_date = "18/10/2021';
    programming_language = "Flask HTML CSS";
    css_framework = "Bootstrap";
  }
