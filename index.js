de81e2233cfd98fdb92f0ccd761e111a0e484105
Badri Vishal <bv7887393@gmail.com>
2021-10-19 18:38:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
- function portfolio_flask() {
-   name = "portfolio-flask"
-   description = "Badri Vishal's Personal Portfolio Website Built with Python Framework Flask Bootstrap and ❤️";
-   expected_date = "18/10/2021';
-   programming_language = "Flask HTML CSS";
-   css_framework = "Bootstrap";
+function portfolio_flask() {
+    repo = "portfolio-flask"
+    description = "Badri Vishal's Personal Portfolio Website Built with Python Framework Flask Bootstrap and ❤️";
+    expected_date = "18/10/2021";
+    programming_language = "Flask HTML CSS";
+    css_framework = "Bootstrap";
 }
 
 function cp_solutions() {
-   name = "cp-solutions"
-   description = "Competitive Programming Solutions in Python/C++. Solutions of CP Websites like AtCoder, CodeChef, and many more.";
-   expected_date = "19/10/2021";
-   programming_language = "C++ Python";
+    name = "cp-solutions"
+    description = "Competitive Programming Solutions in Python/C++. Solutions of CP Websites like AtCoder, CodeChef, and many more.";
+    expected_date = "19/10/2021";
+    programming_language = "C++ Python";
 }
